a98706d939d5bbe5a8b0d408dc52b18785f10a20
Marino Missiroli <marino.missiroli@cern.ch>
2026-03-02 07:23:29 +0100

    JetMETPerformanceAnalysisDriver: add Pt500 categories

--- NTupleAnalysis/src/JetMETPerformanceAnalysisDriver.cc
+++ NTupleAnalysis/src/JetMETPerformanceAnalysisDriver.cc
@@ -21,11 +21,11 @@ void JetMETPerformanceAnalysisDriver::init() {
   jecA_.init(getOption("jecA_filePath"));
 
   jetCategoryLabels_ = {
-      "_EtaIncl",  //"_EtaInclPt0", "_EtaInclPt1", "_EtaInclPt2", "_EtaInclPt3", "_EtaInclPt4", "_EtaInclPt5",
-      "_Eta2p5",   //"_Eta2p5Pt0",  "_Eta2p5Pt1",  "_Eta2p5Pt2",  "_Eta2p5Pt3",  "_Eta2p5Pt4",  "_Eta2p5Pt5",
-      "_HB",       //"_HBPt0",      "_HBPt1",      "_HBPt2",      "_HBPt3",      "_HBPt4",      "_HBPt5",
-      "_HE",       //"_HEPt0",      "_HEPt1",      "_HEPt2",      "_HEPt3",      "_HEPt4",      "_HEPt5",
-      "_HF",       //"_HFPt0",      "_HFPt1",      "_HFPt2",      "_HFPt3",      "_HFPt4",      "_HFPt5",
+      "_EtaIncl",  "_EtaInclPt500", //"_EtaInclPt0", "_EtaInclPt1", "_EtaInclPt2", "_EtaInclPt3", "_EtaInclPt4", "_EtaInclPt5",
+      "_Eta2p5",   "_Eta2p5Pt500",  //"_Eta2p5Pt0",  "_Eta2p5Pt1",  "_Eta2p5Pt2",  "_Eta2p5Pt3",  "_Eta2p5Pt4",  "_Eta2p5Pt5",
+      "_HB",       "_HBPt500",      //"_HBPt0",      "_HBPt1",      "_HBPt2",      "_HBPt3",      "_HBPt4",      "_HBPt5",
+      "_HE",       "_HEPt500",      //"_HEPt0",      "_HEPt1",      "_HEPt2",      "_HEPt3",      "_HEPt4",      "_HEPt5",
+      "_HF",       "_HFPt500",      //"_HFPt0",      "_HFPt1",      "_HFPt2",      "_HFPt3",      "_HFPt4",      "_HFPt5",
   };
 
   // histogram: events counter
@@ -103,6 +103,8 @@ bool JetMETPerformanceAnalysisDriver::jetBelongsToCategory(const std::string& ca
   bool ret{false};
   if (categLabel == "_EtaIncl") {
     ret = (jetAbsEta < 5.0);
+  } else if (categLabel == "_EtaInclPt500") {
+    ret = (jetAbsEta < 5.0) and (jetPt < 500.);
   } else if (categLabel == "_EtaInclPt0") {
     ret = (jetAbsEta < 5.0) and (20. <= jetPt) and (jetPt < 60.);
   } else if (categLabel == "_EtaInclPt1") {
@@ -119,6 +121,8 @@ bool JetMETPerformanceAnalysisDriver::jetBelongsToCategory(const std::string& ca
 
   else if (categLabel == "_Eta2p5") {
     ret = (jetAbsEta < 2.5);
+  } else if (categLabel == "_Eta2p5Pt500") {
+    ret = (jetAbsEta < 2.5) and (jetPt < 500.);
   } else if (categLabel == "_Eta2p5Pt0") {
     ret = (jetAbsEta < 2.5) and (20. <= jetPt) and (jetPt < 60.);
   } else if (categLabel == "_Eta2p5Pt1") {
@@ -135,6 +139,8 @@ bool JetMETPerformanceAnalysisDriver::jetBelongsToCategory(const std::string& ca
 
   else if (categLabel == "_HB") {
     ret = (jetAbsEta < 1.3);
+  } else if (categLabel == "_HBPt500") {
+    ret = (jetAbsEta < 1.3) and (jetPt < 500.);
   } else if (categLabel == "_HBPt0") {
     ret = (jetAbsEta < 1.3) and (20. <= jetPt) and (jetPt < 60.);
   } else if (categLabel == "_HBPt1") {
@@ -151,6 +157,8 @@ bool JetMETPerformanceAnalysisDriver::jetBelongsToCategory(const std::string& ca
 
   else if (categLabel == "_HE") {
     ret = (1.3 <= jetAbsEta) and (jetAbsEta < 3.0);
+  } else if (categLabel == "_HEPt500") {
+    ret = (1.3 <= jetAbsEta) and (jetAbsEta < 3.0) and (jetPt < 500.);
   } else if (categLabel == "_HEPt0") {
     ret = (1.3 <= jetAbsEta) and (jetAbsEta < 3.0) and (20. <= jetPt) and (jetPt < 60.);
   } else if (categLabel == "_HEPt1") {
@@ -167,6 +175,8 @@ bool JetMETPerformanceAnalysisDriver::jetBelongsToCategory(const std::string& ca
 
   else if (categLabel == "_HF") {
     ret = (3.0 <= jetAbsEta) and (jetAbsEta < 5.0);
+  } else if (categLabel == "_HFPt500") {
+    ret = (3.0 <= jetAbsEta) and (jetAbsEta < 5.0) and (jetPt < 500.);
   } else if (categLabel == "_HFPt0") {
     ret = (3.0 <= jetAbsEta) and (jetAbsEta < 5.0) and (20. <= jetPt) and (jetPt < 60.);
   } else if (categLabel == "_HFPt1") {
