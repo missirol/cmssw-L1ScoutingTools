62db16b6a5a7f978d034260e9e323c053f915401
Marino Missiroli <marino.missiroli@cern.ch>
2026-03-06 22:21:33 +0100

    test/testL1ScoutConfig: basic runtime test for a bare L1S config

---
+++ Reconstruction/test/testL1ScoutConfig/testL1ScoutConfig.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+[ $# -eq 1 ] || exit 1
+
+JOBLABEL=tmp_l1Scout
+INPUTFILE=/eos/user/m/missirol/l1s_data_250306/run398183/run398183_ls0261_index000000.raw
+RUNNUMBER=398183
+MAXORBITS=10
+
+wget "${1}" -O "${JOBLABEL}"_cfg.py
+
+sed -i "s|(!%BU_BASE_DIRS_ALL%!)||g" "${JOBLABEL}"_cfg.py
+sed -i "s|(!%BU_BASE_DIRS_NUM_STREAMS%!)||g" "${JOBLABEL}"_cfg.py
+sed -i "s|(!%FILE_BROKER_HOST%!)|''|g" "${JOBLABEL}"_cfg.py
+sed -i "s|(!%PRESCALE%!)|1|g" "${JOBLABEL}"_cfg.py
+
+cat <<@EOF >> "${JOBLABEL}"_cfg.py
+process.source.fileListMode = True
+process.source.fileNames = ['${INPUTFILE}']
+
+process.EvFDaqDirector.runNumber = ${RUNNUMBER}
+process.EvFDaqDirector.baseDir = '.'
+process.EvFDaqDirector.buBaseDir = './tmp'
+process.EvFDaqDirector.buBaseDirsAll = [process.EvFDaqDirector.buBaseDir.value()]
+process.EvFDaqDirector.buBaseDirsNumStreams = [1]
+process.EvFDaqDirector.useFileBroker = False
+
+process.options.numberOfThreads = 1
+process.options.numberOfStreams = 0
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+
+process.maxEvents.input = ${MAXORBITS}
+
+for outModLabel in process.outputModules_():
+    outMod = getattr(process, outModLabel)
+    setattr(process, outModLabel, cms.OutputModule("PoolOutputModule",
+        **outMod.parameters_(),
+        fileName = cms.untracked.string( f"${JOBLABEL}_output_{outModLabel}.root" )
+    ))
+    outMod = getattr(process, outModLabel)
+    del outMod.SelectEvents
+@EOF
+
+rm -rf tmp run"${RUNNUMBER}"
+
+cmsRun "${JOBLABEL}"_cfg.py 2>&1 | tee "${JOBLABEL}"_cfg.log
+
+rm -rf tmp run"${RUNNUMBER}"
