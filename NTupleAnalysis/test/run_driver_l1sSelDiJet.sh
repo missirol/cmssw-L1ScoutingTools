e6f257c79d4c3b24bb997e645bc4cc93c78698d9
Marino Missiroli <marino.missiroli@cern.ch>
2026-06-17 15:43:21 +0200

    NTupleAnalysis: small update to test/run_driver_l1sSelDiJet.sh

--- NTupleAnalysis/test/run_driver_l1sSelDiJet.sh
+++ NTupleAnalysis/test/run_driver_l1sSelDiJet.sh
@@ -2,7 +2,7 @@
 
 batch_driver.py \
   -p L1ScoutSelDiJetAnalysisDriver \
-  -i "${CMSSW_BASE}"/src/L1ScoutingTools/NanoAOD/test/tmpout2/L1ScoutingSelection_Run2026D/*.root \
+  -i out1_l1sReNano/L1ScoutingSelection_*/*.root \
   -o out2_l1sSelDiJet/jobs \
   --JobFlavour espresso \
   -n 1000000
