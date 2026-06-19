9032323e6f366ada0da10caee0004f6571af9f92
Marino Missiroli <marino.missiroli@cern.ch>
2026-06-19 12:49:10 +0200

    NTupleAnalysis: added func l1s_clang_format to env.sh

--- NTupleAnalysis/test/env.sh
+++ NTupleAnalysis/test/env.sh
@@ -7,6 +7,10 @@ fi
 
 export L1SADIR=${PWD}
 
+l1s_clang_format() {
+  clang-format -i --style file:"${CMSSW_BASE}"/src/.clang-format "${CMSSW_BASE}"/src/L1ScoutingTools/*/*/*.{h,cc}
+}
+
 export PATH=${L1SADIR}:${PATH}
 
 export PYTHON3PATH=${PWD}:${PWD}/common:${PYTHON3PATH}
